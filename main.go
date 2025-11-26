283c182405403ac558ee5adaea63679adec8dbc2
Lucas da Cunha <luques.cunha@gmail.com>
2025-11-25 21:04:31 -0300

    adicionando titulares

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var titular string = "Lucas"
+	var numeroAgencia int = 1234
+	var numeroConta int = 567890
+	var saldo float64 = 1000.50
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+	var titular2 string = "Luciene"
+	var numeroAgencia2 int = 555
+	var numeroConta2 int = 444333
+	var saldo2 float64 = 2500.75
+
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
 }
